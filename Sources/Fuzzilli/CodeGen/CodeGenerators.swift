fed1698a2a4d6f3085856e25b301f6879402799e
Raphael Herouart <rherouart@google.com>
2026-07-03 10:54:03 +0200

    [generator] Loops accepting anything should have no element type set
    Bug: 524562043
    Change-Id: Idea3856321cdcfa50c15e38c01b4a0743d418bd6
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9500780
    Commit-Queue: Raphaël Hérouart <rherouart@google.com>
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>

--- Sources/Fuzzilli/CodeGen/CodeGenerators.swift
+++ Sources/Fuzzilli/CodeGen/CodeGenerators.swift
@@ -152,14 +152,14 @@ func makeForInOfLoopGenerator(
         assert(!isAsyncIteration, "async for in is invalid")
         requiredType = .object()
     case .forOf:
-        let elementType: ILType =
+        let elementType: ILType? =
             switch usingType {
             case .awaitUsing:
                 .asyncDisposable()
             case .using:
                 .disposable()
             case .none:
-                .jsAnything
+                nil
             }
 
         if isAsyncIteration {

d64719fee9d2075c182ae668cd222a56408f953f
Raphael Herouart <rherouart@google.com>
2026-09-03 17:03:13 +0200

    [Fuzzilli] Enable compile time safety for FuzzILLifter
    Similar to  JavaScriptLifter.swift or Instruction.swift, this switch
    statement has no condition, it is supposed to handle every opcode. Swift
    by default requires all switch statements to be exhaustive, offering
    compile time safety rather than a runtime error.
    
    Bug: 556210739
    Change-Id: I6e748990d62fc6eef1e72841cfe9966af219294e
    Reviewed-on: https://chrome-internal-review.googlesource.com/c/v8/fuzzilli/+/9775275
    Reviewed-by: Matthias Liedtke <mliedtke@google.com>
    Commit-Queue: Raphaël Hérouart <rherouart@google.com>

--- Sources/Fuzzilli/Lifting/FuzzILLifter.swift
+++ Sources/Fuzzilli/Lifting/FuzzILLifter.swift
@@ -1839,9 +1839,6 @@ public class FuzzILLifter: Lifter {
             w.emit("\(output()) <- WasmJSStringCompare \(input(0)), \(input(1))")
         case .wasmStringConstant(let op):
             w.emit("\(output()) <- WasmStringConstant '\(op.value)'")
-
-        default:
-            fatalError("No FuzzIL lifting for this operation!")
         }
 
     }
